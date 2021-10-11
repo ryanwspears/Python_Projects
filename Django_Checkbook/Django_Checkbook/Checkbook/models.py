7fbc64d82a7770ade2dd60f5d93151e945d58c99
ryanwspears <spearswesley5@gmail.com>
2021-10-10 19:35:27 -0500

    Edited Checkbook

--- Django_Checkbook/Django_Checkbook/Checkbook/models.py
+++ Django_Checkbook/Django_Checkbook/Checkbook/models.py
@@ -12,11 +12,14 @@ class Account(models.Model):
         return self.first_name + ' ' + self.last_name
 
 
+TransactionTypes = [('Deposit', 'Deposit'), ('Withdrawal', 'Withdrawal')]
+
+
 class Transaction(models.Model):
     date = models.DateField()
-    type = models.CharField(max_length=10)
+    type = models.CharField(max_length=10, choices=TransactionTypes)
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     description = models.CharField(max_length=100)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
-    Transaction = models.Manager()
+    Transactions = models.Manager()
